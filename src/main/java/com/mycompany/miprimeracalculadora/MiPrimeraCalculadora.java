2f28ddf7a03d4137b3488a4c5593b551cd0495c5
caleb <calebizqui@gmail.com>
2024-01-05 15:10:48 -0500

    first commit

---
+++ src/main/java/com/mycompany/miprimeracalculadora/MiPrimeraCalculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.miprimeracalculadora;
+import jframe.Calculadora;
+
+/**
+ *
+ * @author caleb
+ */
+public class MiPrimeraCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora view = new Calculadora();
+        view.setVisible(true);
+    }
+}
